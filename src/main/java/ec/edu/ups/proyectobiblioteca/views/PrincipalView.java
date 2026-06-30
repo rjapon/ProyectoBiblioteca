e0ae3ea1586c45125df34ddf145ea792c0c6e501
rjapon <richardjv666@gmail.com>
2026-06-30 18:05:31 -0500

    Commit

--- src/main/java/ec/edu/ups/proyectobiblioteca/views/PrincipalView.java
+++ src/main/java/ec/edu/ups/proyectobiblioteca/views/PrincipalView.java
@@ -52,6 +52,9 @@ public class PrincipalView extends javax.swing.JFrame {
         ActualizarUsuarioMenuItem = new javax.swing.JMenuItem();
         BuscarUsuarioMenuItem = new javax.swing.JMenuItem();
         ListarUsuarioMenuItem = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -143,6 +146,17 @@ public class PrincipalView extends javax.swing.JFrame {
 
         jMenuBar2.add(UsuariosMenu);
 
+        jMenu1.setText("Idioma");
+
+        jMenuItem1.setText("Español");
+        jMenuItem1.addActionListener(this::jMenuItem1ActionPerformed);
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Inglés");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar2.add(jMenu1);
+
         setJMenuBar(jMenuBar2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -186,6 +200,10 @@ public class PrincipalView extends javax.swing.JFrame {
     private void ListarPrestamoMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ListarPrestamoMenuItemActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_ListarPrestamoMenuItemActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,6 +254,9 @@ public class PrincipalView extends javax.swing.JFrame {
     private javax.swing.JMenu PrestamoMenu;
     private javax.swing.JMenu UsuariosMenu;
     private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
